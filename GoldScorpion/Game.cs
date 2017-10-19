e674abe50c13b616ffc35f52c2c825f2e728b3fe
Patrick <kaorti@gmail.com>
2017-10-19 15:40:24 -0700

    game class done, working on round logic

--- GoldScorpion/Game.cs
+++ GoldScorpion/Game.cs
@@ -1,12 +1,63 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GoldScorpion
 {
     class Game
     {
-        static void Main(string[] args)
+        public List<Player> players;
+        public string winner;
+        public string first;
+        private Player firstPlayer;
+
+        public Game(int playerNumber)
         {
-            Console.WriteLine("Game Logic!");
+            firstPlayer = new Player("You");
+            players.Add(firstPlayer);
+            for (int i = 0; i < playerNumber; i++)
+            {
+                players.Add(new Player($"cpu{i}"));
+            }
+            while (winner == null)
+            {
+                Round current = new Round(players, firstPlayer);
+                winner = current.winner;
+                firstPlayer = current.next; 
+            }
+            System.Console.WriteLine("The Winner is: {0}", winner);
+        }
+    }
+    class Round
+    {
+        public string winner = null;
+        public Player next;
+        Dictionary<int, Player> bids = new Dictionary<int, Player>();
+        List<Player> currentPlayers;
+
+        public Round(List<Player> players, Player firstPlayer)
+        {
+            currentPlayers = players;
+            foreach (Player player in players)
+            {
+                player.Hand();
+                player.pile = null;
+            }
+            next = firstPlayer;
+        }
+        private void GetBids()
+        {
+            foreach (Player player in currentPlayers)
+            {
+                System.Console.WriteLine("How many rocks can you flip without finding a scorpion?");
+                System.Console.WriteLine("The highest bid so far is: {0} by {1}", bids.Keys.Max(), bids[bids.Keys.Max()]);
+                Int32.TryParse(Console.ReadLine(), out int attemptedBid);
+                bids.Add(attemptedBid, player);
+            }
+        }
+        private void flipCards()
+        {
+            
         }
     }
 }
