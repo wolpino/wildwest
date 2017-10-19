6f73f2bd82f9d910eefaffbfaeddbf8d94846123
Patrick <kaorti@gmail.com>
2017-10-19 16:10:39 -0700

    flip and bid methods complete

--- GoldScorpion/Game.cs
+++ GoldScorpion/Game.cs
@@ -55,9 +55,37 @@ namespace GoldScorpion
                 bids.Add(attemptedBid, player);
             }
         }
-        private void flipCards()
+        private void flipCards(int maxBid)
         {
-            
+            int flipsLeft = maxBid;
+            int totalFlipped = 0;
+            while (totalFlipped <  maxBid)
+            {
+                System.Console.WriteLine("Choose a player whose cards you'll flip next");
+                for (int i = 0; i < currentPlayers.Count; i++)
+                {
+                    System.Console.WriteLine("{0}. {1}, cards in pile: {2}", i,  currentPlayers[i].name, currentPlayers[i].pile.Count);
+                }
+                Int32.TryParse(Console.ReadLine(), out int chosenPlayer);
+                flipsLeft = currentPlayers[chosenPlayer].flip(maxBid);
+                if (flipsLeft == -1)
+                {
+                    System.Console.WriteLine("A Scorpion!");
+                    bids[maxBid].Discard();
+                    break;
+                }
+                else if(flipsLeft == 0) 
+                {
+                    System.Console.WriteLine("You make it to town with all your gold!");
+                    bids[maxBid].points += 1;
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine("You have {0} flips left", flipsLeft);
+                    totalFlipped = maxBid - flipsLeft;
+                }
+            }
         }
     }
 }
